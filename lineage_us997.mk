435b01d9bdc14d9648e1a07b56a74e0cfd3c8e21
Kyu <33372279+kyunal@users.noreply.github.com>
2019-12-29 13:27:29 +0100

    initial lineage-17.0 from havoc branch

--- lineage_us997.mk
+++ lineage_us997.mk
@@ -18,15 +18,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Havoc stuff.
-$(call inherit-product, vendor/havoc/config/common.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from us997 device
 $(call inherit-product, device/lge/us997/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := us997
-PRODUCT_NAME := havoc_us997
+PRODUCT_NAME := lineage_us997
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-US997
 PRODUCT_MANUFACTURER := LGE
