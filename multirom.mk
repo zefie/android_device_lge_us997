0cf7703b502b613b5037dfae8ad057d6f93a7011
zefie <zefie@zefie.net>
2020-02-20 15:54:58 -0500

    fix aospext

--- multirom.mk
+++ multirom.mk
@@ -1,7 +1,7 @@
 ifeq ($(TARGET_OS_VARIANT),)
    # Scans the vendor directory to see which supported rom is synced into the tree.
    DEVICE_SUPPORTED_LINEAGE_ROMS := lineage carbon havoc aosp
-   TARGET_OS_VARIANT := $(strip $(foreach R, $(DEVICE_SUPPORTED_LINEAGE_ROMS),$(if $(wildcard vendor/$(R)/config/common_full_phone.mk),$(R))))
+   TARGET_OS_VARIANT := $(strip $(foreach R, $(DEVICE_SUPPORTED_LINEAGE_ROMS),$(if $(wildcard vendor/$(R)/config/common.mk),$(R))))
 endif
 
 
